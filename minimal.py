b80d20ce3c410244d4b91535441aca8ec8804ed7
mdaniel-devonte <57999069+mdaniel-devonte@users.noreply.github.com>
2020-01-06 10:23:07 -0500

    Update minimal.py

--- minimal.py
+++ minimal.py
@@ -4,6 +4,17 @@ import pygame
 # Initialize pygame so it runs in the background and manages things
 pygame.init()
 
+#Colors
+red = pygame.Color(255,0,0)
+orange = pygame.Color(255,128,0)
+yellow = pygame.Color(255,155,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
+purple = pygame.Color(255,8,255)
+
+color_index=0
+colors=[red]
+
 # Create a display. Size must be a tuple, which is why it's in parentheses
 screen = pygame.display.set_mode( (400, 300) )
 
@@ -11,7 +22,9 @@ screen = pygame.display.set_mode( (400, 300) )
 while True:
     # do something for each event in the event queue (list of things that happen)
     for event in pygame.event.get():
+        print(event)
         # Check to see if the current event is a QUIT event
         if event.type == pygame.QUIT:
             # If so, exit the program
             sys.exit()
+       
